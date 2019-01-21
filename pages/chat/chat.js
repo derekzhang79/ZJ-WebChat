2a9cf0d5d7473c45bd0d2162e1e4856e325573b7
yufei <yu.fei@cesgroup.com.cn>
2019-01-21 17:08:21 +0800

    BUG修改

--- pages/chat/chat.js
+++ pages/chat/chat.js
@@ -8,9 +8,16 @@ Page({
     showTopTips: false,
     errorMsg: '有错',
 
-    HouseLevels: [],
+    HouseLevels: [
+      { name: '省级综合档案馆', value: '0' },
+      { name: '地市级综合档案馆', value: '1' },
+      { name: '区县级综合档案馆', value: '2' }
+    ],
     HouseLevelIndex: 0,
-
+    type: '0',
+    cityList: [],
+    cityListIndex: 0,
+    cityId: '',
     HouseList: [],
     HouseListIndex: 0,
 
@@ -24,33 +31,48 @@ Page({
   },
   HouseLevelChange: function (e) {
     this.setData({
-      HouseLevelIndex: e.detail.value
+      HouseLevelIndex: e.detail.value,
+      type: this.data.HouseLevels[e.detail.value].value
     })
-    let id = this.data.HouseLevels[e.detail.value].daj_id
-    this.getHouseList(id)
+    if (this.data.type == '0' || this.data.type == '1') {
+      this.getHouseList()
+    } else {
+      this.getCityList().then(res => {
+        this.data.cityId = res[0].daj_id
+        this.getQXHouseList()
+      })
+    }
   },
 
   HouseListChange: function (e) {
     this.setData({
       HouseListIndex: e.detail.value
     })
-    let id = this.data.HouseList[e.detail.value].daj_id
-    this.getLyMethods(id)
   },
 
-  getHouseLevel: function () { // 获取档案馆级别
+  cityListChange: function (e) {
+    this.setData({
+      cityListIndex: e.detail.value,
+      cityId: this.data.cityList[e.detail.value].daj_id
+    })
+    this.getQXHouseList()
+  },
+
+  getHouseList: function () {  // 获取对应的档案馆列表
     let that = this
     return new Promise(function (resolve, reject) {
       wx.request({
-        'url': 'https://www.zjdafw.gov.cn/kgcx/lankgcx/xcxBorrow!getborrowTree',
-        data: {},
+        url: 'https://www.zjdafw.gov.cn/kgcx/lankgcx/xcxBorrow!getTreeByType',
+        data: {
+          type: that.data.type
+        },
         header: {
           'Cookie': wx.getStorageSync('sessionid')
         },
         success(res) {
           resolve(res.data)
           that.setData({
-            HouseLevels: res.data
+            HouseList: res.data
           })
         },
         fail() {
@@ -65,13 +87,42 @@ Page({
     })
   },
 
-  getHouseList: function (parentId) {  // 获取对应的档案馆列表
+  getCityList: function () {  // 获取对应的地市列表
     let that = this
     return new Promise(function (resolve, reject) {
       wx.request({
-        'url': 'https://www.zjdafw.gov.cn/kgcx/lankgcx/xcxBorrow!getTreeByParentId',
+        url: 'https://www.zjdafw.gov.cn/kgcx/lankgcx/xcxBorrow!getTreeByType',
         data: {
-          daj_id: parentId
+          type: that.data.type
+        },
+        header: {
+          'Cookie': wx.getStorageSync('sessionid')
+        },
+        success(res) {
+          resolve(res.data)
+          that.setData({
+            cityList: res.data
+          })
+        },
+        fail() {
+          reject(res)
+          wx.showToast({
+            title: '获取地市列表失败，请刷新页面重新获取',
+            icon: 'warn',
+            duration: 1500
+          })
+        }
+      })
+    })
+  },
+
+  getQXHouseList: function () {  // 获取对应的档案馆列表
+    let that = this
+    return new Promise(function (resolve, reject) {
+      wx.request({
+        url: 'https://www.zjdafw.gov.cn/kgcx/lankgcx/xcxBorrow!getTreeByParentId',
+        data: {
+          daj_id: that.data.cityId
         },
         header: {
           'Cookie': wx.getStorageSync('sessionid')
@@ -155,11 +206,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getHouseLevel().then(res => { // 同步执行，为了使用第一个请求的数据
-      this.getHouseList(res[0].daj_id).then(res => {
-
-      })
-    }) 
+    this.getHouseList() 
   },
 
   /**
